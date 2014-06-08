553a15de931b11535ecde28071cce8e744d94263
zizoujab <zied.jabalah@gmail.com>
2014-06-08 02:57:11 +0100

    added single view for blog

--- src/Disycs/BlogBundle/Controller/DefaultController.php
+++ src/Disycs/BlogBundle/Controller/DefaultController.php
@@ -6,8 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('DisycsBlogBundle:Default:index.html.twig', array('name' => $name));
+    	$posts = $this->get('doctrine')->getRepository('DisycsBlogBundle:Post')->findAll();
+        return $this->render('DisycsBlogBundle:Default:index.html.twig',array('posts'=>$posts));
     }
 }
